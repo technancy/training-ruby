719d190d7cccc566b4d0d28fda1d5c900dd300b2
technancy <nancytechy@gmail.com>
2017-05-20 21:52:55 +0100

    Kata: Guess My Number

---
+++ katas/guess-my-number/guess_my_number.rb
@@ -0,0 +1,36 @@
+class NumberGuesser
+
+	def initialize
+		@max = 100
+		@min = 1
+	end
+
+	def guess
+		@guess = (@max + @min) /2
+	end
+
+	def lower
+		@max = @guess -1
+	end
+
+	def higher
+		@min = @guess + 1
+	end
+
+
+end
+
+def guess_my_number
+  @guesser = NumberGuesser.new
+  @guesser.guess
+end
+
+def higher
+  @guesser.higher
+  @guesser.guess
+end
+
+def lower
+  @guesser.lower
+  @guesser.guess
+end
